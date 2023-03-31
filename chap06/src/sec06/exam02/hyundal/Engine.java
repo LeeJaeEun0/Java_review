a8a0dc6b656ccc9d20520cfaa4e3cb467b279d97
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-03-31 16:35:00 +0900

    upload

---
+++ chap06/src/sec06/exam02/hyundal/Engine.java
@@ -0,0 +1,5 @@
+package sec06.exam02.hyundal;
+
+public class Engine {
+
+}
---
+++ chap06/src/sec06/exam03/package2/C.java
@@ -0,0 +1,9 @@
+package sec06.exam03.package2;
+
+import sec06.exam03.package1.*;
+
+public class C {
+	//A a; //A에 컴파일 오류가 발생하는 것이 맞음
+	B b;
+	
+}
